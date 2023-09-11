7cefac09daf4db041f0e73790b52fb6f58dda778
ootaketetsuyuki <tetsuyukiotake@forthewin.co.jp>
2023-09-11 18:41:06 +0900

    OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+
   get 'messages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root to: "messages#index"
+
+  devise_for :users
+  root to: "rooms#index"
+  resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
+
 end
